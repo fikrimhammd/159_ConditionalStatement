cf1ba1dc27c4d2f6e6b87546fe988e2bd0e5c7eb
fikrimhammd <fikri.kdi60@gmail.com>
2024-03-05 16:16:14 +0700

    Add project files.

---
+++ P 2.2/P 2.2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int nBilangan;
+	string status;
+
+	srand(time(0));
+
+	nBilangan = rand() % 6;
+
+	if (nBilangan % 2 == 0) {
+		status = "Genap";
+
+	}
+	else {
+		status = "Ganjil";
+	}
+
+	cout << "Bilangan akhir = " << nBilangan << endl;
+	cout << "Bilangan adalah = " << status << endl;
+}
---
+++ P 2.3/P 2.3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int nBilanganA;
+	int nBilanganB;
+	string status;
+
+	srand(time(0));
+
+	nBilanganA = rand() % 6;
+	nBilanganB = rand() % 2;
+
+	if (nBilanganA == nBilanganB) {
+		status = "Sama Besar";
+	}
+	else if (nBilanganB) {
+		status = "Bilangan A lebih besar dari Bilangan B";
+	}
+	else {
+		status = "Bilangan A lebih kecil dari Bilangan B";
+	}
+
+	cout << "Bilangan pertama adalah " << nBilanganA << endl;
+	cout << "Bilangan kedua adalah " << nBilanganB << endl;
+	return 0;
+}
